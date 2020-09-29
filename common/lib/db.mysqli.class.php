1254dedcc29206ea7c09894612629da6d35dae9e
Kim Jinhyeok <derrick11@naver.com>
2020-09-29 11:36:07 +0900

    -원격 서버에 있는 db연결 및 테스트

---
+++ common/lib/db.mysqli.class.php
@@ -0,0 +1,55 @@
+<?php
+
+
+class DB
+{
+    protected $host = "jbstv.synology.me:3307";
+
+    protected $user = "a4b1";
+    protected $password = "Last41game!";
+
+    protected $charset = "utf8";
+    protected $port = "3307";
+
+    protected $dbName = "a4b1";
+
+    //싱글톤 패턴
+    protected function __construct() {}
+
+    //싱글톤 패턴
+    public static function getInstance() {
+        static $inst = null;
+        if($inst == null) $inst = new DB();
+        return $inst;
+    }
+
+    public function MysqliConnect() {
+        $dbflag = "NO";
+        $con = mysqli_connect($this->host,$this->user,$this->password,'');
+        $con->set_charset($this->charset);
+        if (!$con) die("Connection failed: " . mysqli_connect_error());
+
+        $sql = "show databases;";
+        $result = mysqli_query($con,$sql) or die('Error: ' . mysqli_error($con));
+        while ($row = mysqli_fetch_row($result)) {
+            if ($row[0] === "a4b1") {
+                $dbflag = "OK";
+                break;
+            }
+        }
+
+        if ($dbflag === "NO") {
+            $sql = "CREATE database a4b1";
+            if (mysqli_query($con, $sql)) echo '<script>alert("a4b1 데이터베이스가 생성되었습니다.");</script>';
+            else echo "실패원인" . mysqli_error($con);
+        }
+
+        $dbcon = mysqli_select_db($con, "a4b1") or die('Error:' . mysqli_error($con));
+
+//        $sql = "create table test (no int primary key not null)";
+//        $result = mysqli_query($con,$sql) or die('Error: ' . mysqli_error($con));
+
+
+        return $con;
+    }
+}
