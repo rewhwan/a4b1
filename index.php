29a7af8757f35cbab92aa7264470a9f7375fd82b
Kim Jinhyeok <derrick11@naver.com>
2020-09-29 11:36:41 +0900

    -메인페이지 추가

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-    
+    require $_SERVER['DOCUMENT_ROOT']."/a4b1/common/lib/db.mysqli.class.php";
+    //싱글톤 객체 불러오기
+    $db = DB::getInstance();
+    $dbcon = $db->MysqliConnect();
+?>
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <title>A4B1's Game Box</title>
+        <link rel="stylesheet" href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/common/css/common.css?ver=1">
+    </head>
+    <body>
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT']."/a4b1/common/lib/header.php";?>
+        </header>
+    </body>
+</html>
