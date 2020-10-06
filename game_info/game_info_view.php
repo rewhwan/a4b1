d39cf79e6ad9873a08686e8851af9e208ce988f3
kimkwanwoo <kkte01@naver.com>
2020-10-06 10:35:20 +0900

    헤더부분 수정

--- game_info/game_info_view.php
+++ game_info/game_info_view.php
@@ -36,3 +36,18 @@
     }
     
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/common/css/common.css?ver=1">
+    <script src="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/common/js/common.js?ver=1"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT']."/a4b1/common/lib/header.php";?>
+    </header>
+</body>
+</html>
