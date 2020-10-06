525fb1942e460b9c95c07f4fb24fd62c068a954f
Kim Jinhyeok <derrick11@naver.com>
2020-10-06 13:14:28 +0900

    로그인 화면 UI 변경

--- login/index.php
+++ login/index.php
@@ -11,20 +11,18 @@
         <header>
             <?php include $_SERVER['DOCUMENT_ROOT']."/a4b1/common/lib/header.php";?>
         </header>
-        <div id="login_form">
-            <form name="login_form" action="login.php" method="post" onkeypress="prevent(event)">
-                <ul>
-                    <li><label for="id">아이디</label></li>
-                    <li><input type="text" id="id" name="id" autocomplete="off" onkeypress="enterLogin(event)"></li>
-                    <li><label for="password">비밀번호</label></li>
-                    <li><input type="text" id="password" name="password" autocomplete="off" onkeypress="enterLogin(event)"></li>
-                    <li><input type="submit"></li>
-                </ul>
-            </form>
-        </div>
-
-        <div>
-
+        <div id="login_form_container">
+            <div id="login_form">
+                <h2>LOGIN</h2>
+                <form name="login_form" action="login.php" method="post" onkeypress="prevent(event)">
+                    <ul>
+                        <li><input type="text" id="id" name="id" placeholder="ID" autocomplete="off" onkeypress="enterLogin(event)"></li>
+                        <li><input type="password" id="password" name="password" placeholder="Password" autocomplete="off" onkeypress="enterLogin(event)"></li>
+                        <li><input type="submit" value="로그인"></li>
+                        <li><a>회원가입</a><a>아이디 찾기</a><a>비밀번호 찾기</a></li>
+                    </ul>
+                </form>
+            </div>
         </div>
     </body>
 </html>
