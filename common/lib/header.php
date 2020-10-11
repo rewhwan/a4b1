fdc684e4e82b9bbc933202ad5a52ff57184f7544
Kim Jinhyeok <derrick11@naver.com>
2020-10-12 00:41:34 +0900

    헤더 UI수정 및 기능 추가

--- common/lib/header.php
+++ common/lib/header.php
@@ -1,5 +1,13 @@
 <div id="header_container">
     <div id="top_menu">
+        <div id="logo_container">
+            <a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/index.php">
+                <img src="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/common/img/logo.png" id="logo">
+            </a>
+            <a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/index.php">
+                <span id="logo_span">A4B1's GameBox</span>
+            </a>
+        </div>
         <ul>
             <?php
                 if(isset($_SESSION['id']) && isset($_SESSION['password'])) {
@@ -7,10 +15,12 @@
                     <li>아이디 : <?=$_SESSION['id']?></li>
                     <li>비밀번호 : <?=$_SESSION['password']?></li>
                     <li>admin : <?=$_SESSION['admin']?></li>
-                    <li><a onclick="logoutCheck('<?=$_SERVER['HTTP_HOST']?>')">로그아웃</a></li>
+                    |
+                    <li class="cursor_pointer"><a onclick="logoutCheck('<?=$_SERVER['HTTP_HOST']?>')">로그아웃</a></li>
             <?php }else{?>
-                    <li><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/login/index.php">로그인</a></li>
-                    <li><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/login/index.php">회원가입</a></li>
+                    <li class="cursor_pointer"><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/login/index.php">로그인</a></li>
+                    |
+                    <li class="cursor_pointer"><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/login/index.php">회원가입</a></li>
             <?php }?>
         </ul>
     </div>
