e422b3910604a6f3c08f4171f4a189294247cbf4
Kim Jinhyeok <derrick11@naver.com>
2020-10-12 14:38:49 +0900

    리뷰 등록기능 UI 및 기능 추가

--- game_review/js/review.js
+++ game_review/js/review.js
@@ -45,7 +45,6 @@ window.onload = function() {
 
     //검색버튼 클릭시 실행 함수
     $('#game_search').click(function () {
-        console.log('검색버튼이 눌렸다.')
         $.ajax({
             url: "http://localhost/a4b1/game_review/review_game_search.php",
             type: 'POST',
@@ -54,14 +53,44 @@ window.onload = function() {
                name: $('#game_name').val()
             },
             success: function (data) {
+                let selectorContainer = $('#game_search_result_container');
                 console.log(data);
-                var selector = $('#game_search_result');
-                for(var i in data.data) {
-                    console.log(data.data[i]);
-                    selector.append("<option value="+data.data[i].num+">"+data.data[i].name+"</option>")
+                console.log(data.isSuccess);
+                console.log(data.data);
+                if(data.isSuccess != 0 && data.data != null) {
+                    selectorContainer.css('display','flex');
+                    selectorContainer.empty();
+                    selectorContainer.append("<p>검색결과</p>");
+                    selectorContainer.append("<select id='game_search_result' name='name'></select>");
+                    let selector = $('#game_search_result');
+                    for(var i in data.data) {
+                        console.log(data.data[i]);
+                        selector.append("<option value="+data.data[i].num+">"+data.data[i].name+"</option>")
+                    }
+                }else {
+                    toastr.error('다른검색어로 검색하거나 게임이름을 입력하고 리뷰를 등록해주세요.','게임 검색 결과가 없습니다.');
+                    selectorContainer.css('display','flex');
+                    selectorContainer.empty();
+                    selectorContainer.append("<p>게임이름</p>");
+                    selectorContainer.append("<input type='text' id='game_search_result' name='name'>");
                 }
             }
         });
+    });
+
+    /*리뷰 작성관련 이벤트 함수 등록*/
+    $('#title').on('keypress',function () {
+        //검색어가 입력되어 있으면 검색버튼을 눌러준다.
+        if(event.keyCode == 13) {
+            $('#submit').click();
+        }
+    });
+
+    $('#content').on('keypress',function () {
+        //검색어가 입력되어 있으면 검색버튼을 눌러준다.
+        if(event.keyCode == 13) {
+            $('#submit').click();
+        }
     });
 }
 
@@ -73,32 +102,32 @@ function check_input(){
         return false;
     }
     if(!$("#title").val()){
-        alert("리뷰제목 이 입력되지 않았습니다.");
+        toastr.error('제목이 입력되지 않았습니다',"제목 입력요망", {timeOut: 3000});
         $("#title").focus();
         return false;
     }
     if(!$("#story").val()){
-        alert("스토리 별점 이 입력되지 않았습니다.");
+        toastr.error('스토리 별점 이 입력되지 않았습니다.',"별점 입력요망", {timeOut: 3000});
         $("#story").focus();
         return false;
     }
     if(!$("#graphic").val()){
-        alert("그래픽 별점 이 입력되지 않았습니다.");
+        toastr.error('그래픽 별점 이 입력되지 않았습니다.',"별점 입력요망", {timeOut: 3000});
         $("#graphic").focus();
         return false;
     }
     if(!$("#time").val()){
-        alert("러닝타임 별점 이 입력되지 않았습니다.");
+        toastr.error('러닝타임 별점 이 입력되지 않았습니다.',"별점 입력요망", {timeOut: 3000});
         $("#time").focus();
         return false;
     }
     if(!$("#difficulty").val()){
-        alert("난이도 별점 이 입력되지 않았습니다.");
+        toastr.error('난이도 별점 이 입력되지 않았습니다.',"별점 입력요망", {timeOut: 3000});
         $("#difficulty").focus();
         return false;
     }
     if(!$("#content").val()){
-        alert("리뷰가 입력되지 않았습니다.");
+        toastr.error('리뷰가 입력되지 않았습니다.',"리뷰 입력요망", {timeOut: 3000});
         $("#content").focus();
         return false;
     }
--- game_review/review_insert_form.php
+++ game_review/review_insert_form.php
@@ -1,128 +1,137 @@
 <!doctype html>
 <html lang="ko">
-<?php
-require $_SERVER['DOCUMENT_ROOT']."/a4b1/common/lib/db.mysqli.class.php";
+    <?php
+    require $_SERVER['DOCUMENT_ROOT'] . "/a4b1/common/lib/db.mysqli.class.php";
 
-//싱글톤 객체 불러오기
-$db = DB::getInstance();
-$db->sessionStart();
-$dbcon = $db->connector();
-?>
-<head>
-    <meta charset="UTF-8">
-    <title>게임리뷰</title>
+    //싱글톤 객체 불러오기
+    $db = DB::getInstance();
+    $db->sessionStart();
+    $dbcon = $db->connector();
+    ?>
+    <head>
+        <meta charset="UTF-8">
+        <title>게임리뷰</title>
 
-    <!--Jquery 추가-->
-    <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/js/jquery/jquery-3.5.1.min.js?ver=1"></script>
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
-    <!--헤더 파일 추가-->
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/css/common.css?ver=1">
-    <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/js/common.js?ver=1"></script>
+        <!--Jquery 추가-->
+        <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/js/jquery/jquery-3.5.1.min.js?ver=1"></script>
 
-    <!--리뷰 파일 추가-->
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/game_review/css/review.css">
-    <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/game_review/js/review.js?ver=1"></script>
+        <!--헤더 파일 추가-->
+        <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/css/common.css?ver=1">
+        <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/js/common.js?ver=1"></script>
 
-    <!--alert & toastr 라이브러리 추가-->
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/css/toastr/toastr.min.css?ver=1"/>
-    <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/js/toastr/toastr.min.js?ver=1"></script>
-    <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/js/sweetalert/sweetalert.min.js?ver=1"></script>
+        <!--리뷰 파일 추가-->
+        <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/game_review/css/review.css">
+        <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/game_review/js/review.js?ver=1"></script>
 
-</head>
+        <!--alert & toastr 라이브러리 추가-->
+        <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/css/toastr/toastr.min.css?ver=1"/>
+        <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/js/toastr/toastr.min.js?ver=1"></script>
+        <script src="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/common/js/sweetalert/sweetalert.min.js?ver=1"></script>
 
-<style>
-    
-</style>
+    </head>
+
+    <style>
+
+    </style>
 
 
-<body>
-    <header>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . "/a4b1/common/lib/header.php"; ?>
-    </header>
-    <div id="form_container">
-        <form id="review_form" name="review_insert_form" method="POST" action="review_insert.php" enctype="multipart/form-data" >
-            <h2>리뷰 작성하기</h2>
-            <ul>
-                <li>
-                    <p>게임</p>
-                    <input type="text" id="game_name" name="game_name" placeholder="리뷰할 게임의 이름을 입력하여 검색">
-                    <button type="button" id="game_search" name="">검색</button>
-                </li>
-                <li>
-                    <p>검색결과</p>
-                    <select id="game_search_result" name="name"></select>
-                </li>
-                <li>
-                    <p>제목</p> <input type="text" id="title" name="title">
-                </li>
-                <li>
-                    <div class="story">
-                        <span class="starR1">별1_왼쪽</span>
-                        <span class="starR2">별1_오른쪽</span>
-                        <span class="starR1">별2_왼쪽</span>
-                        <span class="starR2">별2_오른쪽</span>
-                        <span class="starR1">별3_왼쪽</span>
-                        <span class="starR2">별3_오른쪽</span>
-                        <span class="starR1">별4_왼쪽</span>
-                        <span class="starR2">별4_오른쪽</span>
-                        <span class="starR1">별5_왼쪽</span>
-                        <span class="starR2">별5_오른쪽</span>
-                        <input type="hidden" id="story" name="story" value="0">
-                    </div>
-                    <div class="graphic">
-                        <span class="starR1">별1_왼쪽</span>
-                        <span class="starR2">별1_오른쪽</span>
-                        <span class="starR1">별2_왼쪽</span>
-                        <span class="starR2">별2_오른쪽</span>
-                        <span class="starR1">별3_왼쪽</span>
-                        <span class="starR2">별3_오른쪽</span>
-                        <span class="starR1">별4_왼쪽</span>
-                        <span class="starR2">별4_오른쪽</span>
-                        <span class="starR1">별5_왼쪽</span>
-                        <span class="starR2">별5_오른쪽</span>
-                        <input type="hidden" id="graphic" name="graphic" value="0">
-                    </div>
-                </li>
-                <li>
-                <div class="time">
-                        <span class="starR1">별1_왼쪽</span>
-                        <span class="starR2">별1_오른쪽</span>
-                        <span class="starR1">별2_왼쪽</span>
-                        <span class="starR2">별2_오른쪽</span>
-                        <span class="starR1">별3_왼쪽</span>
-                        <span class="starR2">별3_오른쪽</span>
-                        <span class="starR1">별4_왼쪽</span>
-                        <span class="starR2">별4_오른쪽</span>
-                        <span class="starR1">별5_왼쪽</span>
-                        <span class="starR2">별5_오른쪽</span>
-                        <input type="hidden" id="time" name="time" value="0">
-                    </div>
-                    <div class="difficulty">
-                        <span class="starR1">별1_왼쪽</span>
-                        <span class="starR2">별1_오른쪽</span>
-                        <span class="starR1">별2_왼쪽</span>
-                        <span class="starR2">별2_오른쪽</span>
-                        <span class="starR1">별3_왼쪽</span>
-                        <span class="starR2">별3_오른쪽</span>
-                        <span class="starR1">별4_왼쪽</span>
-                        <span class="starR2">별4_오른쪽</span>
-                        <span class="starR1">별5_왼쪽</span>
-                        <span class="starR2">별5_오른쪽</span>
-                        <input type="hidden" id="difficulty" name="difficulty" value="0">
-                    </div>
-                </li>
-                <li>리뷰남기기 <textarea name="content" id="content" cols="30" rows="10"></textarea></li>
-                <li><p>이미지 첨부하기</p><input type="file" id="new_file" name="new_file[]" multiple accept="image/*"></li>
-            </ul>
-            <button type="button">취소</button>
-           <input type="button" onclick="check_input()" value="등록">
-        </form>
-    </div>
+    <body>
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . "/a4b1/common/lib/header.php"; ?>
+        </header>
+        <div id="form_container">
+            <form id="review_form" name="review_insert_form" method="POST" action="review_insert.php"
+                  enctype="multipart/form-data">
+                <h2>리뷰 작성하기</h2>
+                <ul>
+                    <li>
+                        <p>게임검색</p>
+                        <input type="text" id="game_name" name="game_name" placeholder="리뷰할 게임의 이름을 입력하여 검색">
+                        <button type="button" id="game_search" class="btn btn-skype">검색</button>
+                    </li>
+                    <li id="game_search_result_container">
+                        <p>검색결과</p>
+                        <select id="game_search_result" name="name"></select>
+                    </li>
+                    <li>
+                        <p>제목</p> <input type="text" id="title" name="title" placeholder="제목을 입력해주세요.">
+                    </li>
+                    <li>
+                        <p>항목별 점수</p>
+                        <div id="star_container">
+                            <div class="story">
+                                <p>스토리</p>
+                                <span class="starR1">별1_왼쪽</span>
+                                <span class="starR2">별1_오른쪽</span>
+                                <span class="starR1">별2_왼쪽</span>
+                                <span class="starR2">별2_오른쪽</span>
+                                <span class="starR1">별3_왼쪽</span>
+                                <span class="starR2">별3_오른쪽</span>
+                                <span class="starR1">별4_왼쪽</span>
+                                <span class="starR2">별4_오른쪽</span>
+                                <span class="starR1">별5_왼쪽</span>
+                                <span class="starR2">별5_오른쪽</span>
+                                <input type="hidden" id="story" name="story" value="0">
+                            </div>
+                            <div class="graphic">
+                                <p>그래픽</p>
+                                <span class="starR1">별1_왼쪽</span>
+                                <span class="starR2">별1_오른쪽</span>
+                                <span class="starR1">별2_왼쪽</span>
+                                <span class="starR2">별2_오른쪽</span>
+                                <span class="starR1">별3_왼쪽</span>
+                                <span class="starR2">별3_오른쪽</span>
+                                <span class="starR1">별4_왼쪽</span>
+                                <span class="starR2">별4_오른쪽</span>
+                                <span class="starR1">별5_왼쪽</span>
+                                <span class="starR2">별5_오른쪽</span>
+                                <input type="hidden" id="graphic" name="graphic" value="0">
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div id="star_container">
+                            <div class="time">
+                                <p>러닝타임</p>
+                                <span class="starR1">별1_왼쪽</span>
+                                <span class="starR2">별1_오른쪽</span>
+                                <span class="starR1">별2_왼쪽</span>
+                                <span class="starR2">별2_오른쪽</span>
+                                <span class="starR1">별3_왼쪽</span>
+                                <span class="starR2">별3_오른쪽</span>
+                                <span class="starR1">별4_왼쪽</span>
+                                <span class="starR2">별4_오른쪽</span>
+                                <span class="starR1">별5_왼쪽</span>
+                                <span class="starR2">별5_오른쪽</span>
+                                <input type="hidden" id="time" name="time" value="0">
+                            </div>
+                            <div class="difficulty">
+                                <p>난이도</p>
+                                <span class="starR1">별1_왼쪽</span>
+                                <span class="starR2">별1_오른쪽</span>
+                                <span class="starR1">별2_왼쪽</span>
+                                <span class="starR2">별2_오른쪽</span>
+                                <span class="starR1">별3_왼쪽</span>
+                                <span class="starR2">별3_오른쪽</span>
+                                <span class="starR1">별4_왼쪽</span>
+                                <span class="starR2">별4_오른쪽</span>
+                                <span class="starR1">별5_왼쪽</span>
+                                <span class="starR2">별5_오른쪽</span>
+                                <input type="hidden" id="difficulty" name="difficulty" value="0">
+                            </div>
+                        </div>
+                    </li>
+                    <li><p>리뷰내용</p><textarea name="content" id="content" cols="30" rows="10"></textarea></li>
+                    <li><p>이미지 파일</p><input type="file" id="new_file" name="new_file[]" multiple accept="image/*"></li>
+                </ul>
+                <button type="button">취소</button>
+                <button id="submit" onclick="check_input()" type="button">등록</button>
+            </form>
+        </div>
 
-    <footer>
-
-    </footer>
-
-</body>
+    </body>
 
 </html>
