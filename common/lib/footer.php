51fb043a2200e008c23b747b94adb8354b8b5075
Kim Jinhyeok <derrick11@naver.com>
2020-10-15 09:14:16 +0900

    푸터 수정

--- common/lib/footer.php
+++ common/lib/footer.php
@@ -1,21 +1,42 @@
 <div id="footer_container">
-    <div>
+    <div id="logo_footer_container">
         <img src="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/common/img/logo.png" id="logo_footer">
+        <h3>A4B1's GameBox</h3>
     </div>
     <div id="content">
         <div id="corp_container">
+            <h3>정보</h3>
             <ul>
-                <li>이용약관</li>
-                <li>개인정보취급방침</li>
+                <li><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/footer/terms_of_use.php">이용약관</a></li>
+                <li><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/footer/privacy_policy.php">개인정보취급방침</a></li>
             </ul>
         </div>
         <div id="function_container">
+            <h3>사이트맵</h3>
             <ul>
-                <li>메인</li>
-                <li>게임소개</li>
-                <li>게임리뷰</li>
-                <li>공지사항</li>
+                <li><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/index.php">메인</a></li>
+                <li><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/game_info/index.php">게임소개</a></li>
+                <li><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/game_review/index.php">게임리뷰</a></li>
+                <li><a href="http://<?=$_SERVER['HTTP_HOST']?>/a4b1/notice/index.php">공지사항</a></li>
             </ul>
         </div>
+    </div>
+    <div id="corp_info">
+        <h3>A4B1's GameBox 정보</h3>
+        <ul>
+            <li>대표 김진혁</li>
+            <li>사업자등록번호 000-00-00000</li>
+            <li>남양주시 호평동 늘을3로 00, 000호</li>
+            <li>통신판매업신고 제0000-남양주호평0000</li>
+        </ul>
+    </div>
+    <div id="developer_info">
+        <h3>개발자 정보</h3>
+        <ul>
+            <li>Developer 김관우</li>
+            <li>Developer 김병곤</li>
+            <li>Developer 김진혁</li>
+            <li>Developer 이민혁</li>
+        </ul>
     </div>
 </div>
