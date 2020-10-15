dae1263d42db0c7794b3f22cd73dfc2adc75942f
Leeminhyeck <lbhaudwo@naver.com>
2020-10-15 16:48:55 +0900

    헤더 UI 수정 및 관리자 페이지 슬라이드 이미지 관리자 구현

---
+++ admin/admin_menu.php
@@ -0,0 +1,44 @@
+<div id="title_container">
+    <div>
+        <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/admin/">
+            <span id="title" class="gfont_1">A4B1</span>
+            <span id="title_sub" class="gfont_3">Admin Page</span>
+        </a>
+    </div>
+    <div id="title_function">
+        <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/">
+            <span class="gfont_3">사이트로 돌아가기</span>
+        </a>
+    </div>
+</div>
+<div>
+    <div id="side_menu">
+        <ul>
+            <li>
+                <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/admin/admin_slide.php">
+                    <span class="gfont_3">메인 슬라이드 관리<span>
+                </a>
+            </li>
+            <li>
+                <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/admin/css/admin.css?ver=1">
+                    <span class="gfont_3">메인 슬라이드 관리<span>
+                </a>
+            </li>
+            <li>
+                <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/admin/css/admin.css?ver=1">
+                    <span class="gfont_3">메인 슬라이드 관리<span>
+                </a>
+            </li>
+            <li>
+                <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/admin/css/admin.css?ver=1">
+                    <span class="gfont_3">메인 슬라이드 관리<span>
+                </a>
+            </li>
+            <li>
+                <a href="http://<?= $_SERVER['HTTP_HOST'] ?>/a4b1/admin/css/admin.css?ver=1">
+                    <span class="gfont_3">메인 슬라이드 관리<span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ admin/message.class.php
@@ -0,0 +1,41 @@
+<?php
+class message
+{
+    public $errorMsg = array();
+    public $errorIds = array();
+    public $warningMsg = array();
+    public $isSuccess = true;
+    public $data = null;
+
+    public function add($kind, $msg) {
+        switch ($kind) {
+            case 'errorMsg' :
+                array_push($this->errorMsg, $msg);
+                $this->isSuccess = false;
+                break;
+
+            case 'errorIds' :
+                array_push($this->errorIds, $msg);
+                $this->isSuccess = false;
+                break;
+
+            case 'warningMsg' :
+                array_push($this->warningMsg, $msg);
+                break;
+
+            case 'data' :
+                $this->data = $msg;
+                break;
+        }
+    }
+
+    public function error($errorIds,$errorMsg) {
+        $this->add("errorIds",$errorIds);
+        $this->add("errorMsg",$errorMsg);
+        return $this;
+    }
+
+    public function printJson() {
+        echo json_encode($this);
+    }
+}
